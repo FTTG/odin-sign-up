16f353fcc5ad98bce9c1af6ea3b6b832728cc016
FTTG <32749931+FTTG@users.noreply.github.com>
2023-05-18 12:19:32 -0600

    Improve validation styling

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const pass = document.querySelector('#pass');
 const conpass = document.querySelector('#con-pass');
+const btn = document.querySelector('#completed');
+
+const inputs = document.querySelectorAll('input');
 
 conpass.addEventListener('input', () => {
     console.log('cambiando');
@@ -9,4 +12,14 @@ conpass.addEventListener('input', () => {
     else {
         conpass.setCustomValidity('');
     }
+});
+
+inputs.forEach(field => {
+    field.addEventListener('blur', () => {
+        field.required = true;
+    });
+});
+
+btn.addEventListener('click', () => {
+    inputs.forEach(field => field.required = true);
 });
