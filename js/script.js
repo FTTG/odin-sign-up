1643e331fdf3e199fe9bda594e03b5d5cafc672c
FTTG <32749931+FTTG@users.noreply.github.com>
2023-05-18 22:12:04 -0600

    Add validation labels below inputs

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const pass = document.querySelector('#pass');
 const conpass = document.querySelector('#con-pass');
 const btn = document.querySelector('#completed');
+const errorMsg = document.querySelector('.error-msg');
 
 const inputs = document.querySelectorAll('input');
+
+let firstcheck = false;
 
 conpass.addEventListener('input', () => {
     console.log('cambiando');
@@ -17,6 +20,25 @@ conpass.addEventListener('input', () => {
 inputs.forEach(field => {
     field.addEventListener('blur', () => {
         field.required = true;
+        if (field.checkValidity() == false) {
+            field.nextElementSibling.classList.add('show-error');
+            console.log('add error');
+        }
+        else {
+            field.nextElementSibling.classList.remove('show-error');
+            console.log('remove error');
+        }
+        field.classList.add('checked');
+    });
+});
+
+inputs.forEach(field => {
+    field.addEventListener('input', () => {
+        console.log('input registered');
+        if (field.classList.contains('checked') && field.checkValidity() == true) {
+            console.log(field.checkValidity());
+            field.nextElementSibling.classList.remove('show-error');
+        }
     });
 });
 
